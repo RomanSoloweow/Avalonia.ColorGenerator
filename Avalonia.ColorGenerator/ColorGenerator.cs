9de214af2a92ed1ad6d083e1cf50ad71715d5189
GMIKE <roman.soloweow333@ya.ru>
2021-02-20 15:20:37 +0300

    version 1.0

---
+++ Avalonia.ColorGenerator/ColorGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Avalonia.Media;
+using Metsys.Bson;
+
+namespace Avalonia.ColorGenerator
+{
+    public static class ColorGenerator
+    {
+        public static void Init()
+        {
+            HelperColors.Init(); 
+        }
+        
+        static Random Random { get; set; } = new();
+        
+        public static Color Next()
+        {
+           var colorNumber =  Random.Next(0, HelperColors.ColorsUint.Count + 1);
+           var uintValue = HelperColors.ColorsUint[(KnownColor) colorNumber];
+           
+           return Color.FromUInt32(uintValue);
+        }
+    }
+}
